6d00e5b2646da9959578d639f90df72b760ee144
sdplion <sdplion@outlook.com>
2020-10-09 20:11:37 +0300

    git push origin master

---
+++ docs/precache-manifest.a6d60d726e40d5242aa224098bdf7653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bc58b7b36445ac826f4c628abe8ad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c15692f31301538e218",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f3a8912e9639c9ea3ccc",
+    "url": "/static/js/2.1bbe048d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1bbe048d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c15692f31301538e218",
+    "url": "/static/js/main.169fc0af.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  }
+]);
