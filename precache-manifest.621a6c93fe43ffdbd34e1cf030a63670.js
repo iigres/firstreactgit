95f18e52d3aa5653aecf0d5e7cc33ab8149b3528
sdplion <sdplion@outlook.com>
2020-10-09 21:36:03 +0300

    Updates

---
+++ precache-manifest.621a6c93fe43ffdbd34e1cf030a63670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24aa276c755170a992bdaa81649b8c1",
+    "url": "/firstreactgit/index.html"
+  },
+  {
+    "revision": "b77accd92ef77f79200f",
+    "url": "/firstreactgit/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/firstreactgit/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/firstreactgit/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b77accd92ef77f79200f",
+    "url": "/firstreactgit/static/js/main.2b7489ad.chunk.js"
+  },
+  {
+    "revision": "3a39027ed610ef5c1352",
+    "url": "/firstreactgit/static/js/runtime-main.4a5d11d1.js"
+  }
+]);
